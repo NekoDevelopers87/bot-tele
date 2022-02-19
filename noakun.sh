f39bb1681239ce131ac5e511d749590b931e0f27
NekoDevelopers87 <100033707+NekoDevelopers87@users.noreply.github.com>
2022-02-19 21:57:04 +0700

    Add files via upload

---
+++ noakun.sh
@@ -0,0 +1,11 @@
+python main.py +6283110359199 
+sleep 5s
+python main.py +6283149926698 
+sleep 5s
+python main.py +6283142971384 
+sleep 5s
+python main.py +6283163078202 
+sleep 5s
+python main.py +6283108783253 
+sleep 5s
+python main.py +6283802083728 
